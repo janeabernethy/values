40db00ab320e3abd2c6a97d8bbaeddcb243b640e
Jane Abernethy <jane.abernethy@hotmail.com>
2020-02-24 13:16:27 +1100

    Updates

---
+++ precache-manifest.afaae0f19224ae186b47c2601385d348.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d756358ee96735a1ddfcb76647e28ac",
+    "url": "/values/index.html"
+  },
+  {
+    "revision": "9c1afee859e252383adb",
+    "url": "/values/static/css/main.b7fef0fc.chunk.css"
+  },
+  {
+    "revision": "fc5c8f9b5faff72e0efc",
+    "url": "/values/static/js/2.1d79fadf.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/values/static/js/2.1d79fadf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c1afee859e252383adb",
+    "url": "/values/static/js/main.a11fe076.chunk.js"
+  },
+  {
+    "revision": "df6a140e998c41c03920",
+    "url": "/values/static/js/runtime-main.fec2084b.js"
+  },
+  {
+    "revision": "2715209e956f26f3845ff5d4f2afbb15",
+    "url": "/values/static/media/BackgroundImage.2715209e.png"
+  },
+  {
+    "revision": "0153d036bd44b753f5ad4a91d4c17209",
+    "url": "/values/static/media/Divide.0153d036.png"
+  }
+]);
