316305738bc9f28148ed1c95f6a194c6635a01de
Jane Abernethy <jane.abernethy@hotmail.com>
2020-04-05 12:49:14 +1000

    get blocks showing

--- src/Seesaw.tsx
+++ src/Seesaw.tsx
@@ -4,6 +4,12 @@ import seesawBase from './Images/Seesaw/SeesawBase.png';
 import swingyPart from './Images/Seesaw/SwingyPart.png'; 
 import sideA from './Images/Seesaw/SideA.png'; 
 import sideB from './Images/Seesaw/SideB.png'; 
+import blockShadedA from './Images/Seesaw/BlockShadedHalfA.png'; 
+import blockShadedB from './Images/Seesaw/BlockShadedHalfB.png'; 
+import blockFullA from './Images/Seesaw/BlockFullHalfA.png'; 
+import blockFullB from './Images/Seesaw/BlockFullHalfB.png'; 
+import blockFull from './Images/Seesaw/BlockFull.png'; 
+import blockShaded from './Images/Seesaw/BlockShaded.png'; 
 
 export type SeesawProps = { 
     items: Array<string>
@@ -19,9 +25,20 @@ export class Seesaw extends React.Component<SeesawProps, SeesawState> {
 
 
     render() {
+ 
+
+
+
         return (
         <div className="seesawContent">
             <div className="seesawWrapper">
+                <div className="seesawOptionsA">
+                    {
+                        this.props.items.map((object, i) => 
+                          i %3 == 0 ? <Seesaw1Option value={object}/> : <Seesaw2Option items={this.props.items} index={i} />            
+                        )
+                    }
+                </div>
                 <div className="seesawBottomSection">
                     <div className="seesawSideA"><img className="seesawSideAImg" src={sideA} /></div>
                     <div className="seesawSideB"><img className="seesawSideBImg" src={sideB} /></div>
@@ -33,3 +50,50 @@ export class Seesaw extends React.Component<SeesawProps, SeesawState> {
         )
     }
 }
+
+interface Seesaw1OptionProps {
+    value: string;
+  }
+  
+  class Seesaw1Option extends React.Component<Seesaw1OptionProps>  {
+    render () {
+      return (
+        <div className="options1Set">
+            <div className="optionBlock blockC"><img src={blockShaded} /></div>
+        </div>
+      )
+    }
+  }
+
+  interface Seesaw2OptionProps {
+    items: Array<string>
+    index: number
+  }
+  
+  class Seesaw2Option extends React.Component<Seesaw2OptionProps>  {
+    render () {
+        console.log("IN 2")
+        console.log(this.props.index % 3.0)
+    var shouldRender = this.props.index % 3 < 2
+    var hasNext = (this.props.index + 1) < this.props.items.length
+    if (shouldRender == true && hasNext) {
+        return (
+            <div className="options2Set">
+                <div className="optionBlock blockA"><img src={blockFull} /></div>
+                <div className="optionBlock blockB"><img src={blockShaded} /></div>
+            </div>
+          )
+    }
+    else if(shouldRender == true) {
+        return (
+        <div className="options1Set">
+            <div className="optionBlock blockC"><img src={blockShaded} /></div>
+        </div>
+        )
+    }
+    else {
+        return <div></div>
+    }
+      
+    }
+  }
